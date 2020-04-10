966678b87668c962c466abfe2a544e62bfa9fefb
Jesse Markowitz <jess.markowitz@gmail.com>
2020-04-10 19:41:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-var recipes = {ingredient:"amount"}
+var recipes = { ingredient: "amount" }
+
+function updateObjectWithKeyAndValue(recipes, ingredient, amount) {
+  return Object.assign( {}, recipes, { [ingredient]: amount} )
+}
