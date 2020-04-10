835a1a172dc6cf387f90d93d833a7f69f3f7629f
Jesse Markowitz <jess.markowitz@gmail.com>
2020-04-10 19:45:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var recipes = { ingredient: "amount" }
 function updateObjectWithKeyAndValue(recipes, ingredient, amount) {
   return Object.assign( {}, recipes, { [ingredient]: amount} )
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(recipes, ingrdient, amount) {
+  object.assign(recipes, { [ingredient]: amount} )
+}
